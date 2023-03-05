6ff4c62f164641b4653e10488e43c60332d12548
Yurii <99128333+CrocodileGenna@users.noreply.github.com>
2023-03-05 12:25:40 +0200

    first copy footer

--- src/index.js
+++ src/index.js
@@ -1,10 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class Counter extends Component {
+  state = { isOpen: false };
+
+  toggle = () => {
+    this.setState(state => ({ isOpen: !state.isOpen }));
+  };
+
+  render() {
+    const { isOpen } = this.state;
+    const { children } = this.props;
+    console.log(this.isOpen);
+    return (
+      <div>
+        <button onClick={this.toggle}>{isOpen ? 'Hide' : 'Show'}</button>
+        {isOpen && children}
+      </div>
+    );
+  }
+  // static defaultProps = {
+  //   step: 1,
+  //   initialValue: 0,
+  // };
+
+  // state = {
+  //   value: this.props.initialValue,
+  // };
+
+  // handleIncrement = evt => {
+  //   console.log('Increment button was clicked!', evt); // працює
+  //   console.log('this.props: ', this.props); // працює
+  // };
+
+  // handleDecrement = evt => {
+  //   console.log('Decrement button was clicked!', evt); // працює
+  //   console.log('this.props: ', this.props); // працює
+  // };
+
+  // render() {
+  //   const { step } = this.props;
+
+  //   return (
+  //     <div>
+  //       <span>{this.state.value}</span>
+  //       <button type="button" onClick={this.handleIncrement}>
+  //         Increment by {step}
+  //       </button>
+  //       <button type="button" onClick={this.handleDecrement}>
+  //         Decrement by {step}
+  //       </button>
+  //     </div>
+  //   );
+  // }
+}
+
+ReactDOM.render(<Counter step={5} />, document.getElementById('root'));
